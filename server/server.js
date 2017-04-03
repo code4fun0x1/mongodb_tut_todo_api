ca8c547a4930e67b6f75eea2c73793051e72e4b7
code4fun0x1 <shashank0x1@gmail.com>
2017-04-03 22:41:32 +0530

    Tutorial up to mongoose Library

--- server/server.js
+++ server/server.js
@@ -8,36 +8,62 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 //create model
 var Todo = mongoose.model('Todo',{
   text:{
-    type:String
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
   },
   completed:{
-    type:Boolean
+    type:Boolean,
+    default:false
   },
   completedAt:{
-    type:Number
+    type:Number,
+    default:null
   }
 });
 //craete a object from model and save it
-var newTodo = new Todo({
-  text:'Cook Dinner'
-});
-newTodo.save().then((doc)=>{
-  console.log('Saved to database');
-  console.log(doc);
-},(e)=>{
-  console.log('unable to save',e);
-});
+// var newTodo = new Todo({
+//   text:'Cook Dinner'
+// });
+// newTodo.save().then((doc)=>{
+//   console.log('Saved to database');
+//   console.log(doc);
+// },(e)=>{
+//   console.log('unable to save',e);
+// });
 
 //second object
-var secondTodo = new Todo({
-  text:'Paying Game',
-  completed:false,
-  completedAt:123
+// var secondTodo = new Todo({
+//   text:'Paying Game',
+//   completed:false,
+//   completedAt:123
+// });
+//
+// secondTodo.save().then((doc)=>{
+//   console.log('Saved Successful');
+//   console.log(doc);
+// },(e)=>{
+//   console.log(e);
+// });
+
+
+//set up user
+//email-required-trim-settype-minleenth=1
+
+var UserModel = mongoose.model('UserModel',{
+  email:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true,
+  }
 });
 
-secondTodo.save().then((doc)=>{
-  console.log('Saved Successful');
-  console.log(doc);
-},(e)=>{
-  console.log(e);
+var newUser=new UserModel({
+  email:'abc@abc.com'
 });
+newUser.save().then((doc)=>{
+  console.log('Saved Succefully');
+  console.log(doc);
+},(e)=>{console.log(e);});
