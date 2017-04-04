1211e32fce343cb635ed4d37ea584b3c17d8ff50
code4fun0x1 <shashank0x1@gmail.com>
2017-04-04 16:03:27 +0530

    test for get/todos get all todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,11 +5,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos=[
+  {text:"test 1 todos"},
+  {text:"test 2 todos"}
+];
+
+//insertMany() mongoose to insert many item in db
+
 //empty database before every teest
 beforeEach((done)=>{
   Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos); //this will return a promise
     done();
-  });
+  }).then(()=>{done();});
 });
 
 describe('POST/todos',()=>{
@@ -28,7 +36,7 @@ describe('POST/todos',()=>{
         return done(err);
       }
       //model.find() fetches entirer collection
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{
         //console.log(todos);
         //we empty the database so length is 1 always
         expect(todos.length).toBe(1);
@@ -64,11 +72,25 @@ it('should not create a new todos',(done)=>{
       return done(err);
     }
     Todo.find().then((todos)=>{
-      expect(todos.length).toBe(0);
+      expect(todos.length).toBe(2);
       done();
     }).catch((e)=>{
       done(e);
     });
   });
 });
+});
+
+
+//test case for get/all todos
+describe('GET/todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  });
 });
