d448435988dc1a7fa20ed1b26bd76f193a1fd665
code4fun0x1 <shashank0x1@gmail.com>
2017-04-06 18:55:27 +0530

    set up seperate test and deployment env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //give it the conneection asynchonouusly
 //but mongoose handle everything
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports={mongoose};
