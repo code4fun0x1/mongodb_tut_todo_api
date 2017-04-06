6e330882ecf4530c9dddf446e71e164323a47964
code4fun0x1 <shashank0x1@gmail.com>
2017-04-06 16:55:11 +0530

    test cases for delete todos

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,27 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} =require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+//Todo.remove({}) remove all data
+//Todo.remove({key:value}) remove multiple item
+//findOneAndremove and remove one item and return the deleted iteem
+//Todo.findOneAndRemove({key:value})
+//Todo.findByIdandRemove('id')  returns the deleted item
+
+// Todo.remove({}).then(()=>{
+//   consoole.log('Removed all document');
+// });
+//
+// //deleted item gets returned
+// Todo.findOneAndRemove({text:'play games'}).then((todo)=>{
+//   console.log(todo);
+// });
+
+//deleted item gets returned
+//id should be valid
+//if not found then 'null' returned
+Todo.findByIdAndRemove('58e3a51fa0b2c6001179a2f0').then((todo)=>{
+  console.log(todo);
+});
